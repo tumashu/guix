a4492985d4eb5c5e8d05ccae61baaffdf8373e21
Feng Shu <tumashu@163.com>
2022-02-26 04:44:18 +0800

    gnu: mates-utils: Update to 1.26.0.
    * gnu/packages/mate.scm (mate-utils): Update to 1.26.0.

--- gnu/packages/mate.scm
+++ gnu/packages/mate.scm
@@ -1201,7 +1201,7 @@ can be used as backgrounds in the MATE Desktop environment.")
 (define-public mate-utils
   (package
     (name "mate-utils")
-    (version "1.24.0")
+    (version "1.26.0")
     (source
      (origin
        (method url-fetch)
@@ -1209,7 +1209,7 @@ can be used as backgrounds in the MATE Desktop environment.")
                            name "-" version ".tar.xz"))
        (sha256
         (base32
-         "1b16n1628gcsym5mph6lr9x5xm4rgkxsa8xwr2wlx8g2gw2775i1"))))
+         "0bkqj8qwwml9xyvb680yy06lv3dzwkv89yrzz5jamvz88ar6m9bw"))))
     (build-system glib-or-gtk-build-system)
     (native-inputs
      (list gettext-minimal
